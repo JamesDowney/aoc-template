b7e5b404a50d60db9674076fc2b2f18ee70e7ae2
JamesDowney <james.m.downey30@gmail.com>
2022-11-30 23:36:51 -0700

    basic input and output

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,10 @@
+import { bufferToFile, fileToBuffer, print } from "kolmafia";
+
 export default function main(): void {
-  console.log("You have successfully built aoc-template!");
+  const input = fileToBuffer("input.txt").trim().split(/\n/g);
+  const solution = input;
+  solution.forEach((element) => {
+    print(element);
+  });
+  bufferToFile(solution.join("\n"), "output.txt");
 }
